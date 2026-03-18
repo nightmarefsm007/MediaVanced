5023cc244c4646b284250f3a7b037a9f380d7a39
Yogesh Kumar Jamre <58415010+yogesh-hacker@users.noreply.github.com>
2026-03-18 09:30:36 +0530

    Update saicord.py

--- sites/saicord.py
+++ sites/saicord.py
@@ -1,8 +1,10 @@
 import re
+import time
 import base64
 import requests
 import cloudscraper
 from bs4 import BeautifulSoup
+from urllib.parse import urlparse
 
 '''
 Supports:
@@ -22,21 +24,25 @@ class Colors:
 
 # Constants
 base_url = "https://saicord.com/hi/movies/1433-attack-on-finland.html"
-default_domain = "https://saicord.com/"
+parsed_url = urlparse(base_url)
+default_domain = f"{parsed_url.scheme}://{parsed_url.netloc}/"
 headers = {
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
+    "Accept-Language": "en-US,en;q=0.9",
+    "Cache-Control": "no-cache",
+    "Connection": "keep-alive",
     "Referer": default_domain,
-    "User-Agent": "Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Mobile Safari/537.36"
-}
-
-cookies = {
-    "cf_clearance" : "Y8doFuh8iHDmeTtx2BcymH_XUdwUg2rbT7ebwRTmMsQ-1722687625-1.0.1.1-32h0KLviCUYW48bTf3GzGn5MgqW9GMlNCo2t28iFRzW3y2aTMQeLq_iMtmteV4GpfqzpMI.7E1ww6jWGofoRIw"
+    "Upgrade-Insecure-Requests": "1",
+    "User-Agent": "Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/137.0.0.0 Mobile Safari/537.36"
 }
 
 # Create Cloudscraper Session to bypass Cloudflare
 scraper = cloudscraper.create_scraper()
 
-# Fetch response
-response = scraper.get(base_url, headers=headers, cookies=cookies).text
+# Fetch response and try to bypass CF
+response = scraper.get(base_url, headers=headers).text
+if "Just a moment" in response:
+    exit("Cloudflare detected retry again...")
 soup = BeautifulSoup(response,"html.parser")
 
 # Get main script
@@ -45,16 +51,15 @@ script = iframe.find_all("script")
 
 encrypted_data_match = re.search(r'atob\("([^"]*)"\)', script[1].string)
 if not encrypted_data_match:
-    exit(print("No encrypted data found."))
+    exit("No encrypted data found!")
 
 # Decode encrypted data
 encoded_data = encrypted_data_match.group(1);
-decoded_bytes = base64.b64decode(encoded_data)
-decoded_string = decoded_bytes.decode('utf-8')
+decoded_data = base64.b64decode(encoded_data).decode('utf-8')
 
 # Extract video URL
-video_match = re.search(r'file:"([^"]+)"', decoded_string)
-video_url = video_match.group(1) if video_match else exit(print("No video URL found."))
+video_match = re.search(r'file:"([^"]+)"', decoded_data)
+video_url = video_match.group(1) if video_match else exit(print("No video URL found!"))
 
 # Print results
 print("\n" + "#" * 25 + "\n" + "#" * 25)
