60d799fcb60249d48f34d49ccc14cf5333f452fd
Yogesh Kumar Jamre <58415010+yogesh-hacker@users.noreply.github.com>
2026-03-22 11:34:22 +0530

    Fixed for Latest ✅

--- sites/cinesrc.py
+++ sites/cinesrc.py
@@ -14,8 +14,6 @@ Supports:
 https://cinesrc.st/
 '''
 
-# Many thanks to AzartX (https://github.com/AzartX47) for the guidance and support throughout the development of this scraper!
-
 class Colors:
     header = '\033[95m'
     okblue = '\033[94m'
@@ -30,18 +28,18 @@ class Colors:
 
 # Constants
 base_url = 'https://cinesrc.st/embed/movie/1084242'
-user_agent = "Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Mobile Safari/537.36"
+user_agent = "Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/137.0.0.0 Mobile Safari/537.36"
 default_domain = '{uri.scheme}://{uri.netloc}'.format(uri=urlparse(base_url))
 headers = {
     'Referer': default_domain,
     'User-Agent': user_agent
 }
-server_action_token = requests.get('https://script.google.com/macros/s/AKfycbyWasTBsvJ_dZjs2IdlViYYmNRXxRCiVa0kjZrHw4JXlrjyQ7PrQYzo54kDX4SVL9Ne/exec').json().get('key')
+server_action_token = requests.get('https://script.google.com/macros/s/AKfycbz0Nk6EXmwLr-QxTan0EZ1AUKTFG4bxb-9p0BYURnUmyMbQuI-ZF3ybwa8cGg8UXn4w/exec').json().get('key')
 rsa_public_key = """-----BEGIN PUBLIC KEY-----
-MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCPnLvTpbxYFPHBv5TRj8uRaMlh
-yp2ekzUgnyfMopVfnrsPgeC8mmM+tlmebZvDtA/zHGwYoAXViE7oiH57mbhVKrMp
-T8OqE8sLlfppSDcEiLCfRAz8NfGu14gp7Uld9JiGMMeGSDNjtdEbAFD5jArxXbt9
-cBBHg6Y5o40AM60WrwIDAQAB
+MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCFplzWaLwhwOrmKuhe6Vghq4RU
+NBjis9AUEl9Vw6r3CHz8aQGncnNgQ0t1oChO7I7EVs2oKcOrBNfhg+8aP4gURUoU
+Xuk9M8M7OdeVvB2rTxiYIX+mvoslyTuAVRTWTvL5qQu9tZ84PfBCd6vFxku20TmF
+MYYm2zpuXzCI4mmBewIDAQAB
 -----END PUBLIC KEY-----"""
 aes_key_b64 = "JWmlRlgGKC3MLQihZMqx/hW276z1FolQ8QRePYWhn/E="
 
@@ -84,21 +82,46 @@ salt = get_random_bytes(16)
 iv = get_random_bytes(12)
 aes_key = get_random_bytes(32)
 
+# Generate fingerprint
+fingerprint = {
+    'tz': "Asia/Calcutta",
+    'lang': "en-GB",
+    'langs': "en-GB,en-US,en",
+    'pf': "Linux armv81",
+    'cm': True,
+    'dpr': 2,
+    'sw': 360,
+    'sh': 806,
+    'cd': 24,
+    'cvs': "6398717e3beb0e320556d5fb2077d04aaae6041ca9f2e8e6a2e912a156849ee1",
+    'wgl': "ANGLE (ARM, Mali-G57 MC2, OpenGL ES 3.2)|Google Inc. (ARM)",
+    'jit': "0.5999999046325684,0.9000000953674316,0.9000000953674316,0.5999999046325684,0.2999997138977051"
+}
+
 # Prepare payload
 payload = {
-    'v': 1,
-    'ts': timestamp,
-    'ua': user_agent,
-    'nonce': base64.b64encode(nonce).decode(),
-    'salt': base64.b64encode(salt).decode(),
-    'token': token,
-    'path': path
+    '_r': 1.1,
+    '_k': timestamp,
+    '_m': user_agent,
+    '_n1l': "i3Dguw==",
+    '_x': base64.b64encode(nonce).decode(),
+    '_q': base64.b64encode(nonce).decode(),
+    '_j': token,
+    '_n25': "mtD6yA==",
+    '_w': path,
+    '_f': fingerprint
 }
-plaintext = json.dumps(payload).encode()
+payload_array = [item for k, v in payload.items() for item in (k, v)]
+plaintext = json.dumps(payload_array).encode()
+
+# XOR the Payload
+xored = bytearray(len(plaintext))
+for i in range(len(plaintext)):
+    xored[i] = plaintext[i] ^ aes_key[i % len(aes_key)]
 
 # Encrypt Payload using AES-256-GCM
 cipher = AES.new(aes_key, AES.MODE_GCM, nonce=iv)
-encrypted, tag = cipher.encrypt_and_digest(plaintext)
+encrypted, tag = cipher.encrypt_and_digest(xored)
 aes_ciphertext = encrypted + tag
 
 # Encrypt AES Key with RSA‑OAEP SHA256
@@ -112,11 +135,11 @@ iv_b64 = base64.b64encode(iv).decode()
 ciphertext_b64 = base64.b64encode(aes_ciphertext).decode()
 
 # Prepare Encrypted Blob
-encrypted_blob = '.'.join(['v1', encrypted_key_b64, iv_b64, ciphertext_b64])
+encrypted_blob = '~'.join(['v1.1', encrypted_key_b64, iv_b64, ciphertext_b64])
 
 # Prepare Payload and Get Encrypted Streaming Data
 request_payload = [content_id, content_type, season, episode, encrypted_blob, selected_server]
-response = requests.post(base_url, data=json.dumps(request_payload), headers={**headers, 'next-action': server_action_token}).text
+response = requests.post(base_url, data=json.dumps(request_payload), headers={**headers, 'Accept': 'text/x-component', 'next-action': server_action_token, 'Content-Type': 'text/plain;charset=UTF-8'}).text
 
 # Extract encrypted payload line
 encrypted_line = response.splitlines()[1]
